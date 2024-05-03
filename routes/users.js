81b1e15f1f2faf977c0ba0f80b84c4a76a6113c3
Yuriy Vedernikov <yu.piter.gm@gmail.com>
2024-05-03 23:58:16 +0300

    feat: add post routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const usersRouter = require("express").Router();
 
-const { findAllUsers } = require("../middlewares/users");
-const sendAllUsers = require("../controllers/users");
+const { findAllUsers, createUser } = require("../middlewares/users");
+const { sendAllUsers } = require("../controllers/users");
 
 usersRouter.get("/users", findAllUsers, sendAllUsers);
+usersRouter.post("/users", findAllUsers, createUser, sendAllUsers);
 
 module.exports = usersRouter;
