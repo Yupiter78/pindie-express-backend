9c92f2eae657de4281d7d051ad0dfbfd09b8e543
Yuriy Vedernikov <yu.piter.gm@gmail.com>
2024-05-06 21:57:39 +0300

    fix: change auth [controller]

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,35 +1,23 @@
 const users = require("../models/user");
 const bcrypt = require("bcryptjs");
 
-const login = (req, res) => {
+const login = async (req, res) => {
     const { email, password } = req.body;
-    users
-        .findOne({ email })
-        .then((user) => {
-            if (!user) {
-                return Promise.reject(
-                    new Error("НЕ правильные почта или пароль")
-                );
-            }
-            return bcrypt.compare(password, user.password).then((matched) => {
-                if (!matched) {
-                    return Promise.reject(
-                        new Error("Не_правильные почта или пароль")
-                    );
-                }
-                return user;
-            });
-        })
-        .then((user) => {
-            res.status(200).send({
-                _id: user._id,
-                username: user.username,
-                email: user.email
-            });
-        })
-        .catch((error) => {
-            res.status(401).send({ message: error.message });
+    try {
+        const user = await users.findOne({ email });
+        const matched = await bcrypt.compare(password, user.password);
+        if (!user || !matched) {
+            throw new Error("Неправильная почта или пароль");
+        }
+
+        res.json({
+            _id: user._id,
+            username: user.username,
+            email: user.email
         });
+    } catch (error) {
+        res.status(401).json({ message: error.message });
+    }
 };
 
 module.exports = { login };
